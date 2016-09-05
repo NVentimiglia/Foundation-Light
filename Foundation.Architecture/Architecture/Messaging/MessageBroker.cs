cce03ccaf3d8dca3909ed02da39ea431fc1b7e34
Nicholas Ventimiglia <nick@simplesys.us>
2016-09-05 16:21:29 -0700

    cleanup

--- Foundation.Architecture/Architecture/Messaging/MessageBroker.cs
+++ Foundation.Architecture/Architecture/Messaging/MessageBroker.cs
@@ -16,27 +16,12 @@ namespace Foundation.Architecture
     public static class MessageBroker<T>
     {
         public delegate void MessageDelegate(T message);
-
-        public delegate void RoutedMessageDelegate(uint channel, T message);
-
+        
         /// <summary>
         /// Event
         /// </summary>
         public static event MessageDelegate OnMessage = delegate { };
-
-        /// <summary>
-        /// Event
-        /// </summary>
-        public static event RoutedMessageDelegate OnRoutedMessage = delegate { };
-
-        /// <summary>
-        /// sends a message to subscriptions
-        /// </summary>
-        public static void Receive(uint channel, T message)
-        {
-            OnRoutedMessage(channel, message);
-        }
-
+      
         /// <summary>
         /// sends a message to subscriptions
         /// </summary>
@@ -60,22 +45,6 @@ namespace Foundation.Architecture
         {
             OnMessage -= handler;
         }
-
-        /// <summary>
-        /// Adds a route
-        /// </summary>
-        public static void Subscribe(RoutedMessageDelegate handler)
-        {
-            OnRoutedMessage += handler;
-        }
-
-        /// <summary>
-        /// removes a route
-        /// </summary>
-        public static void Unsubscribe(RoutedMessageDelegate handler)
-        {
-            OnRoutedMessage -= handler;
-        }
     }
 
     /// <summary>
@@ -83,32 +52,8 @@ namespace Foundation.Architecture
     /// </summary>
     public static class MessageBroker
     {
-        static Dictionary<Type, Action<ushort, object>> _routedcache = new Dictionary<Type, Action<ushort, object>>();
         static Dictionary<Type, Action<object>> _cache = new Dictionary<Type, Action<object>>();
         
-        /// <summary>
-        /// Notifies listeners of a new message
-        /// </summary>
-        public static void Publish(ushort channel, object message, Type type)
-        {
-            if (!_routedcache.ContainsKey(type))
-            {
-                var info = typeof(MessageBroker<>).MakeGenericType(type).GetMethod("Publish");
-                var func = (Action<ushort, object>)Delegate.CreateDelegate(typeof(Action<ushort, object>), info);
-                _routedcache.Add(type, func);
-            }
-
-            _routedcache[type](channel, message);
-        }
-
-        /// <summary>
-        /// Notifies listeners of a new message
-        /// </summary>
-        public static void Publish<T>(ushort channel, T message)
-        {
-            MessageBroker<T>.Receive(channel, message);
-        }
-
         /// <summary>
         /// Notifies listeners of a new message
         /// </summary>
