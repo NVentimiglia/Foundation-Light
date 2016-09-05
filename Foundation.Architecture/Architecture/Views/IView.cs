fbb53f302415dad0286bcc787e133acc25970487
Nicholas Ventimiglia <nick@avariceonline.com>
2016-09-05 12:22:57 -0700

    Update IView.cs

--- Foundation.Architecture/Architecture/Views/IView.cs
+++ Foundation.Architecture/Architecture/Views/IView.cs
@@ -8,6 +8,11 @@ namespace Foundation.Architecture
     public interface IView : IDisposable
     {
         /// <summary>
+        /// Should Render
+        /// </summary>
+        bool IsVisible { get; set; };
+        
+        /// <summary>
         /// Initializes the view with a viewModel to bind to
         /// </summary>
         void Init(object viewModel);
